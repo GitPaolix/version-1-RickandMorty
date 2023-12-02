0994faadcd3eef9120aa848ac122e754c1788ada
GitPaolix <p.gscheroff@gmail.com>
2023-12-03 00:47:47 +0100

    version 1 RickandMorty

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 
-const url = "https://jsonplaceholder.typicode.com/posts";
 
-const findPostById = async (id) => {
-    try {
-        const res = await fetch(
-            "https://jsonplaceholder.typicode.com/posts/" + id
-        );
-        const post = await res.json();
-        console.log(post);
-    } catch (error) {
-        console.log(error);
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  fetchData();
+});
+const fetchData = async () => {
+// console.log('obteniendo datos...')
+try{
+  loadingData(true);
+
+  const res = await fetch('https://rickandmortyapi.com/api/character');
+  const data = await res.json();
+
+console.log(data);
+pintarCards(data)
+}catch (error){
+   console.log(error)
+}finally{
+    loadingData(false)
+}
+
 };
 
-findPostById(50);
+const pintarCards = data => {
+    const cards = document.getElementById('card-dinamicas')
+    const templateCard = document.getElementById('template-card').content
+    const fragment = document.createDocumentFragment()
+    // console.log(data)
+    data.results.forEach(item => {
+        // console.log(item)
+
+   const clone = templateCard.cloneNode(true)
+   clone.querySelector('h5').textContent = item.name;
+   clone.querySelector('p').textContent = item.species;
+   clone.querySelector('.card-img-top').setAttribute('src',item.image);
+   fragment.appendChild(clone)
+    });
+    cards.appendChild(fragment)
+};
+
+const loadingData = estado => {
+    const loading = document.getElementById('loading')
+    if(estado){
+        loading.classList.remove('d-none')   
+    }else{
+        loading.classList.add('d-none')
+    }
+    
+};
