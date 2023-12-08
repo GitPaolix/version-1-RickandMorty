60cf44dd5a8e6c9ebc36b9350f6bcbf9d48d73b1
GitPaolix <p.gscheroff@gmail.com>
2023-12-08 22:36:10 +0100

    agregué uid

--- app.js
+++ app.js
@@ -5,41 +5,42 @@ const templateEstudiante = document.querySelector(
   "#templateEstudiante"
 ).content;
 const templateProfesor = document.querySelector("#templateProfesor").content;
+const alert = document.querySelector(".alert");
 
 const estudiantes = [];
 const profesores = [];
 
-document.addEventListener('click', e =>{
+document.addEventListener("click", (e) => {
   // console.log(e.target.dataset.nombre);
-  if(e.target.dataset.nombre){
+  if (e.target.dataset.uid) {
     // console.log(e.target.matches(".btn-success"))
-    if(e.target.matches(".btn-success")){
-      estudiantes.map(item =>{
-        if(item.nombre === e.target.dataset.nombre){
-          item.setEstado = true
+    if (e.target.matches(".btn-success")) {
+      estudiantes.map((item) => {
+        if (item.uid === e.target.dataset.uid) {
+          item.setEstado = true;
         }
-        console.log(item)
-        return item
-      })
+        console.log(item);
+        return item;
+      });
     }
-    if(e.target.matches(".btn-danger")){
-      estudiantes.map(item =>{
-        if(item.nombre === e.target.dataset.nombre){
-          item.setEstado = false
+    if (e.target.matches(".btn-danger")) {
+      estudiantes.map((item) => {
+        if (item.nombre === e.target.dataset.nombre) {
+          item.setEstado = false;
         }
-        console.log(item)
-        return item
-      })
-    
+        console.log(item);
+        return item;
+      });
     }
-    Persona.pintarPersonaUI(estudiantes, "Estudiante")
+    Persona.pintarPersonaUI(estudiantes, "Estudiante");
   }
-})
+});
 
 class Persona {
   constructor(nombre, edad) {
     this.nombre = nombre;
     this.edad = edad;
+    this.uid = `${Date.now()}`;
   }
 
   static pintarPersonaUI(personas, tipo) {
@@ -93,8 +94,8 @@ class Estudiante extends Persona {
       ? "Aprobado"
       : "Reprobado";
 
-      clone.querySelector('.btn-success').dataset.nombre = this.nombre
-      clone.querySelector('.btn-danger').dataset.nombre = this.nombre
+    clone.querySelector(".btn-success").dataset.uid = this.uid;
+    clone.querySelector(".btn-danger").dataset.uid = this.uid;
 
     return clone;
   }
@@ -114,8 +115,16 @@ class Profesor extends Persona {
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  alert.classList.add("d-none");
+
   const datos = new FormData(formulario);
   const [nombre, edad, opcion] = [...datos.values()];
+
+  if (!nombre.trim() || !edad.trim() || !opcion.trim()) {
+    console.log("algun dato en blanco");
+    alert.classList.remove("d-none");
+    return;
+  }
 
   if (opcion === "Estudiante") {
     const estudiante = new Estudiante(nombre, edad);
