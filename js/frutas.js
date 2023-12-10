6481d0039a3551fd4f56a0e99cee878c4361d22a
GitPaolix <p.gscheroff@gmail.com>
2023-12-10 13:15:47 +0100

    todo listo

---
+++ js/frutas.js
@@ -0,0 +1,16 @@
+// const sandia = "🍉";
+
+// export default sandia;
+export default function() {
+  console.log("🍌");
+}
+const frutilla = () => {
+  console.log("🍓");
+};
+
+class Fruta {
+  constructor(nombre) {
+    this.nombre = nombre;
+  }
+}
+export { frutilla, Fruta};
